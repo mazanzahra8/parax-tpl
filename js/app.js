c8c126a0695eca6f718bda0548ab565d6fbb593a
unknown <mazanzahra8@gmail.com>
2025-10-25 16:42:20 +0330

    up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
-import { Navigation, Pagination, Grid, Autoplay, Thumbs, EffectCards, FreeMode ,EffectCoverflow } from 'swiper/modules';
+import { Navigation, Pagination, Grid, Autoplay, Thumbs, EffectCards, FreeMode ,EffectCoverflow,EffectFade} from 'swiper/modules';
 
 // import Swiper and modules styles
 import 'swiper/css';
@@ -10,7 +10,8 @@ import 'swiper/css/effect-cards';
 import 'swiper/css/grid';
 import 'swiper/css/autoplay';
 import 'swiper/css/free-mode';
-Swiper.use([Navigation, Pagination, Grid, Autoplay, Thumbs, EffectCards, FreeMode ,EffectCoverflow]);
+import 'swiper/css/effect-fade';
+Swiper.use([Navigation, Pagination, Grid, Autoplay, Thumbs, EffectCards, FreeMode ,EffectCoverflow,EffectFade]);
 window.Swiper = Swiper
 import PureCounter from "@srexi/purecounterjs";
 window.PureCounter = PureCounter;
@@ -20,22 +21,7 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
 
-const reveals = document.querySelectorAll(".reveal");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      } else {
-        entry.target.classList.remove("active");
-      }
-    });
-  },
-  { threshold: 0.2 }
-);
-
-reveals.forEach(el => observer.observe(el));
 
 
 
